9e95e1c4efa83b2ee4a470f33a136420dff63343
Martin Silenus <sileenus@gmail.com>
2012-07-26 17:04:43 +0900

    registration page added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+  def login
+    render :layout => 'login'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Higgins::Application.routes.draw do
+  match '/home/register' => "home#login"
   match '/home/:action', :controller => 'home'
   root :to => "home#index"
 end
